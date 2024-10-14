7321d67ad4c94755bda1a80992f9e42d748c2965
VicToryOsa <27843761@students.lincoln.ac.uk>
2024-10-14 15:34:37 +0000

    updated

--- src/rob2002_project/rob2002_project/move.py
+++ src/rob2002_project/rob2002_project/move.py
@@ -1,37 +1,36 @@
 import rclpy
 from rclpy.node import Node
 
-from std_msgs.msg import String
+from geometry_msgs.msg import Twist
 
-
-class MinimalPublisher(Node):
+class MoverBasic(Node):
 
     def __init__(self):
-        super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
-        timer_period = 0.5  # seconds
+        super().__init__('mover_basic')
+        self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)
+        timer_period = 2.0  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
     def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
+        msg = Twist()
+        msg.linear.x = 0.1
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.get_logger().info('Publishing: "{0}"'.format(msg))
         self.i += 1
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    minimal_publisher = MinimalPublisher()
+    mover_basic = MoverBasic()
 
-    rclpy.spin(minimal_publisher)
+    rclpy.spin(mover_basic)
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    minimal_publisher.destroy_node()
+    mover_basic.destroy_node()
     rclpy.shutdown()
 
 
